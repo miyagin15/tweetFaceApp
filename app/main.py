0985f3cc42624322dc1268052a5045f95cf1df7d
miyagin15 <jump1268@yahoo.co.jp>
2020-07-11 10:09:06 +0900

    nihukura data get

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template
+import nifukura
+
 app = Flask(__name__)
 
+class FaceRecord:
+    def __init___(self, Age, Sex, Face_s, idx):
+        self.Age = Age
+        self.Sex = Sex
+        self.Face_s = Face_s
+        self.idx = idx
+        
 @app.route('/')
-def hello_world():
-    tweets = []
+def tweets_face_app():
+    face_all_data = []
     n_open_tweets = 0
+    face_nihukura_records = nifukura.get_face_data()
+    print(face_nihukura_records)
+    for idx, face_nihukura_record in enumerate(face_nihukura_records):
+        print(face_nihukura_record)
+        face_record = FaceRecord(**face_nihukura_record,idx=idx)
+        face_all_data.append(face_record)
+        break
+    print(face_all_data)
+
     return render_template('base.html', tweets=tweets, n_open_tweets=n_open_tweets)
+
---
+++ app/nifukura.py
@@ -0,0 +1,37 @@
+import urllib.request, urllib.parse
+import json
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+X_NCMB_Application_Key = environ['X-NCMB-Application-Key']
+X_NCMB_Timestamp = environ['X-NCMB-Timestamp']
+X_NCMB_Signature = environ['X-NCMB-Signature']
+
+def get_face_data():
+    # 色々なヘッダーをまとめて付与
+    headers = {
+        "X-NCMB-Application-Key" :X-NCMB-Application-Key,
+        "X-NCMB-Timestamp" :X-NCMB-Timestamp,
+        "X-NCMB-Signature" :X-NCMB-Signature,
+        "Content-Type" :"application/json"
+    }
+    req = urllib.request.Request("https://mbaas.api.nifcloud.com/2013-09-01/classes/No_Login/?order=-createDate&limit=1000", headers=headers)
+    with urllib.request.urlopen(req) as res:
+        res = res.read().decode("utf-8")
+        res = json.loads(res)
+        # print(html)
+        return res["results"]
+
+# main関数
+if __name__ == '__main__':
+    res = get_face_data()
+    #print(data)
+    #print(json.dumps(datas,sort_keys=True, indent=4))
+    #print(datas["results"])
+    #datas
+    #print(res)
+    for idx, face_data in enumerate(res):
+        print(idx)
+        print(face_data)
